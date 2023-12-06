eacfa7a3049bcf57b8297254d337da5e08d6e219
Steven Speck <stvnspk@gmail.com>
2023-12-06 12:37:35 -0800

    adding navbar and homepage content

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 function setLastUpdateText() {
   const milliseconds_since_last_edit = Math.floor(
-    (Date.now() - Date.parse("05 Dec 2023")) / (1000 * 60 * 60 * 24)
+    (Date.now() - Date.parse("06 Dec 2023")) / (1000 * 60 * 60 * 24)
   );
   document.getElementById("update_time").innerHTML =
     "Last updated " + milliseconds_since_last_edit + " days ago";
