fd1db78c63f8f117257d2bb56a4a1159151acd4b
Steven Speck <stvnspk@gmail.com>
2023-12-18 15:50:37 -0600

    new page files structure

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 function setLastUpdateText() {
   const days_since_last_edit = Math.floor(
-    (Date.now() - Date.parse("15 Dec 2023")) / (1000 * 60 * 60 * 24)
+    (Date.now() - Date.parse("18 Dec 2023")) / (1000 * 60 * 60 * 24)
   );
   document.getElementById("update_time").innerHTML =
     "Last updated " + days_since_last_edit + " days ago";
