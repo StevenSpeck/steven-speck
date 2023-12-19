a82a0b3b7f537006390b4fd1d4561ecca7fb044a
Steven Speck <stvnspk@gmail.com>
2023-12-19 18:24:40 -0500

    post template

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 function setLastUpdateText() {
   const days_since_last_edit = Math.floor(
-    (Date.now() - Date.parse("18 Dec 2023")) / (1000 * 60 * 60 * 24)
+    (Date.now() - Date.parse("19 Dec 2023")) / (1000 * 60 * 60 * 24)
   );
+  const day_or_days = days_since_last_edit === 1 ? " day " : " days ";
   document.getElementById("update_time").innerHTML =
-    "Last updated " + days_since_last_edit + " days ago";
+    "Last updated " + days_since_last_edit + day_or_days + "ago";
 }
 
 setTimeout(setLastUpdateText, 50);
